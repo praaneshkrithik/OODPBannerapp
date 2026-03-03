5603b5b3c44f4cffbb640b0018c3fb615648c41d
praaneshkrithik <praaneshkrithik9608@gmail.com>
2026-03-03 10:14:38 +0000

    Add OODPBannerapp3 class with ASCII art output

---
+++ OODPBannerapp3.java
@@ -0,0 +1,13 @@
+public class OODPBannerapp3 {
+	public static void main(String[] args){
+		System.out.println("...");		   
+	    System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " ******  " , " ", "  *****  "));
+		System.out.println(String.join(" "," **   ** " , " ", " **   ** " , " ", " **    **" , " ", " **   ** "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " **    **" , " ", " **      "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " ******  " , " ", "  *****  "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " **      " , " ", "      ** "));
+		System.out.println(String.join(" "," **   ** " , " ", " **   ** " , " ", " **      " , " ", " **   ** "));
+		System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " **      " , " ", "  *****  "));
+    }
+}
+
